51d6d6a9cad98beb7a37efac563942ba7388c857
Taysoren <taysoren@gmail.com>
2016-11-02 06:42:41 -0600

    Added Seed Data, and people Creator. Need to hook it up to the seed method

---
+++ TakeHomeTest/Seeder/PeopleProducer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using TakeHomeTest.Models;
+
+namespace TakeHomeTest.Seeder {
+	class PeopleProducer {
+		public void ProducePeople() {
+			People people = PeopleXMLParser.ParseXML(@"people.xml");
+			if(people != null) {
+				string[] imagePaths = new string[0];
+				try {
+					imagePaths = Directory.GetFiles(@"Pictures");
+				}
+				catch(Exception ex) {
+					Console.WriteLine(string.Format("Error getting images: {0}", ex.Message));
+					return;
+				}
+				var rand = new Random();
+				if(imagePaths != null && imagePaths.Length > 0) {
+					foreach(Person person in people) {
+						var image = File.ReadAllBytes(imagePaths[rand.Next(imagePaths.Length)]);
+						person.Picture = image;
+					}
+				}
+			}
+		}
+	}
+	class People : List<Person> {
+	}
+}
---
+++ TakeHomeTest/Seeder/PeopleXMLParser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace TakeHomeTest.Seeder {
+	public static class PeopleXMLParser {
+		public static People ParseXML(string xmlpath) {
+			if(!File.Exists(xmlpath)) {
+				Console.WriteLine("File does not exist");
+				return null;
+			}
+			try {
+				XmlSerializer serializer = new XmlSerializer(typeof(People));
+				FileStream stream = new FileStream(xmlpath, FileMode.Open, FileAccess.Read);
+				People people = (People)serializer.Deserialize(stream);
+				stream.Close();
+				return people;
+			}
+			catch(InvalidOperationException ex) {
+				Console.WriteLine(string.Format("InvalidOperationException - InnerException:{0}", ex.InnerException.Message));
+				return null;
+			}
+		}
+	}
+}
