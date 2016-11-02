4f54d2996bb51c76674d7e45e7a2cf34e2228b84
Taysoren <taysoren@gmail.com>
2016-11-02 06:10:42 -0600

    Added Context, Database, and first migration

---
+++ TakeHomeTest/Migrations/201611021208531_InitialMigration.cs
@@ -0,0 +1,33 @@
+namespace TakeHomeTest.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.People",
+                c => new
+                    {
+                        PersonID = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Age = c.Int(nullable: false),
+                        Address = c.String(),
+                        PhoneNumber = c.String(),
+                        Email = c.String(),
+                        Interests = c.String(),
+                        Picture = c.Binary(),
+                    })
+                .PrimaryKey(t => t.PersonID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.People");
+        }
+    }
+}
---
+++ TakeHomeTest/Models/PeopleContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace TakeHomeTest.Models {
+	public class PeopleContext : DbContext {
+
+		public DbSet<Person> People { get; set; }
+
+		public PeopleContext()
+			: base("name=PeopleContext") {
+
+		}
+	}
+}
